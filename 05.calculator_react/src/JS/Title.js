f250bc11b595d8765d0a9e2a4a1fccb81c39e8d9
hwangheetae <tejaia@naver.com>
2024-03-11 22:30:34 +0900

    refactor: devide into Components

---
+++ 05.calculator_react/src/JS/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Title() {
+  return (
+    <div className="title">
+      <h1>예산 계산기</h1>
+    </div>
+  );
+}
+
+export default Title;
---
+++ 05.calculator_react/src/JS/TotalAmount.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TotalAmount() {
+  return <div className="total-amount">총지출:</div>;
+}
+
+export default TotalAmount;
