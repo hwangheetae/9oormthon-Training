12d36ab448e7372158baa43f90c0d55080535c53
hwangheetae(laptop) <tejaia@naver.com>
2024-03-13 15:22:19 +0900

    feat: 에러 수정

--- 05.calculator_react/src/JS/TotalAmount.js
+++ 05.calculator_react/src/JS/TotalAmount.js
@@ -2,7 +2,7 @@ import React, { memo } from "react";
 
 const TotalAmount = memo(({ totalAmount }) => {
   return (
-    <div className="text-center my-5 text-2xl color text-right mx-40 font-bold">
+    <div className=" my-5 text-2xl color text-right mx-40 font-bold">
       총 지출: {totalAmount}
     </div>
   );
